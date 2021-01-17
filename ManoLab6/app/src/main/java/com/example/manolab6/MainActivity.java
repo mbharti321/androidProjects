9cecb38d085c5f41a9050674be53f676ac3d4cbe
mbharti321 <mbharti321@gmail.com>
2021-01-17 12:36:17 +0530

    add reset code and some validation

--- ManoLab6/app/src/main/java/com/example/manolab6/MainActivity.java
+++ ManoLab6/app/src/main/java/com/example/manolab6/MainActivity.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
@@ -17,16 +18,15 @@ public class MainActivity extends AppCompatActivity {
     private EditText mUsername;
     private RadioGroup mRadioGroup;
     private CheckBox mCheckBangalore, mCheckChennai, mCheckMumbai, mCheckDelhi;
-    private Button mSubmit, mReset;
-
+    private ImageView mImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mSubmit = findViewById(R.id.btnSubmit);
-        mReset = findViewById(R.id.btnReset);
+        Button mSubmit = findViewById(R.id.btnSubmit);
+        Button mReset = findViewById(R.id.btnReset);
 
         mUsername = findViewById(R.id.editTextUsername);
         mRadioGroup = findViewById(R.id.radioGroup);
@@ -34,11 +34,17 @@ public class MainActivity extends AppCompatActivity {
         mCheckChennai = findViewById(R.id.checkChennai);
         mCheckMumbai = findViewById(R.id.checkMumbai);
         mCheckDelhi = findViewById(R.id.checkDelhi);
+        mImageView = findViewById(R.id.imageView1);
 
 
         mSubmit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                if(mUsername.getText().toString().equals("")){
+                    Toast.makeText(MainActivity.this, "Please enter username",Toast.LENGTH_SHORT).show();
+                    return;
+                }
                 StringBuffer displayMsg = new StringBuffer();
                 displayMsg.append("Username: ");
                 displayMsg.append(mUsername.getText()).append("\n");
@@ -47,11 +53,13 @@ public class MainActivity extends AppCompatActivity {
                 Button ageButton = findViewById(selectedRadio);
                 if(selectedRadio == -1){
                     Toast.makeText(MainActivity.this, "Please select one age group",Toast.LENGTH_SHORT).show();
+                    return;
                 }
                 else{
                     displayMsg.append("Age Group: ");
                     displayMsg.append(ageButton.getText()).append("\n");
                 }
+
                 displayMsg.append("Preferred City: ");
                 if(mCheckBangalore.isChecked()){
                     displayMsg.append(mCheckBangalore.getText());
@@ -65,15 +73,23 @@ public class MainActivity extends AppCompatActivity {
                 if(mCheckDelhi.isChecked()){
                     displayMsg.append(", ").append(mCheckDelhi.getText());
                 }
-
+                mImageView.setVisibility(View.VISIBLE);
                Toast.makeText(MainActivity.this, displayMsg,Toast.LENGTH_SHORT).show();
+
             }
         });
 
         mReset.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(MainActivity.this, "Hi", Toast.LENGTH_SHORT).show();
+//                Toast.makeText(MainActivity.this, "Hi", Toast.LENGTH_SHORT).show();
+                mUsername.setText("");
+                mRadioGroup.clearCheck();
+                mCheckBangalore.setChecked(false);
+                mCheckDelhi.setChecked(false);
+                mCheckMumbai.setChecked(false);
+                mCheckChennai.setChecked(false);
+                mImageView.setVisibility(View.INVISIBLE);
             }
         });
     }
