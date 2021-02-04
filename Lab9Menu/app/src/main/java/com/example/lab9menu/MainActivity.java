4c582c9573c51a65d95c0c088ab34e81dd232ee2
mbharti321 <mbharti321@gmail.com>
2021-02-04 13:47:43 +0530

    update whatsapp sharing

--- Lab9Menu/app/src/main/java/com/example/lab9menu/MainActivity.java
+++ Lab9Menu/app/src/main/java/com/example/lab9menu/MainActivity.java
@@ -6,10 +6,11 @@ import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.core.app.ShareCompat;
 
 import android.annotation.SuppressLint;
-import android.app.Activity;
 import android.content.ComponentName;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Color;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -23,7 +24,7 @@ import com.google.android.material.textfield.TextInputEditText;
 public class MainActivity extends AppCompatActivity {
 
     private ImageButton mibtnSend;
-    TextInputEditText mMessage;
+    TextInputEditText mMessage, mMain_number;
     ConstraintLayout mlayout;
     boolean iscolor = true;
 
@@ -33,14 +34,28 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mMessage = findViewById(R.id.main_textbox);
+        mMessage = findViewById(R.id.main_message);
+        mMain_number = findViewById(R.id.main_number);
         mlayout = (ConstraintLayout) findViewById(R.id.layout1);
         mibtnSend = findViewById(R.id.ibtnSend);
 
         mibtnSend.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                whatsappSend();
+
+                String txtMessage = mMessage.getText().toString();
+                String mob_number = mMain_number.getText().toString();
+                if(mob_number.equals("")){
+                    Toast.makeText(MainActivity.this, "Whatsapp", Toast.LENGTH_SHORT).show();
+                    whatsappSendChooser(txtMessage);
+                }
+                else if (mob_number.length() != 10){
+                    Toast.makeText(MainActivity.this, getString(R.string.not10Digits), Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    Toast.makeText(MainActivity.this, "Whatsapp", Toast.LENGTH_SHORT).show();
+                    whatsappSend(mob_number, txtMessage);
+                }
             }
         });
     }
@@ -61,14 +76,17 @@ public class MainActivity extends AppCompatActivity {
 //                return true;
 
             case R.id.color:
+                Toast.makeText(MainActivity.this,"Change color", Toast.LENGTH_SHORT).show();
                 changeColor();
                 return true;
 
             case R.id.share:
+                Toast.makeText(MainActivity.this, "Share", Toast.LENGTH_SHORT).show();
                 share();
                 return true;
 
             case R.id.exit:
+                Toast.makeText(MainActivity.this, "exit", Toast.LENGTH_SHORT).show();
                 exit();
                 return true;
 
@@ -105,41 +123,48 @@ public class MainActivity extends AppCompatActivity {
                 .startChooser();
     }
 
-//    private void whatsappSend() {
-//        String txt = mMessage.getText().toString();
-//
-//        Intent sendIntent = new Intent();
-//        sendIntent.setAction(Intent.ACTION_SEND);
-//        sendIntent.putExtra(Intent.EXTRA_TEXT, txt);
-//        sendIntent.setType("text/plain");
-//        sendIntent.setPackage("com.whatsapp");
-//
-//        startActivity(sendIntent);
-//
-//    }
 
 
+    private boolean appInstalledOrNot(String url) {
+        PackageManager packageManager = getPackageManager();
+        boolean app_installed;
+
+        try {
+            packageManager.getPackageInfo(url, PackageManager.GET_ACTIVITIES);
+            app_installed = true;
+        } catch ( PackageManager.NameNotFoundException e ) {
+            app_installed = false;
+        }
+        return app_installed;
+    }
+    private void whatsappSendChooser(String txtMessage) {
 
 
-    //https://medium.com/@mujjtahidah/send-whatsapp-to-specific-number-android-studio-96c8caf7422f
-    //    @SuppressLint("NewApi")
-    private void whatsappSend() {
-        String txt = mMessage.getText().toString();
-        try{
-            Intent sendIntent =new Intent("android.intent.action.MAIN");
-            sendIntent.setComponent(new ComponentName("com.whatsapp", "com.whatsapp.Conversation"));
+        boolean installed = appInstalledOrNot("com.whatsapp");
+        if (installed) {
+            Intent sendIntent = new Intent();
             sendIntent.setAction(Intent.ACTION_SEND);
+            sendIntent.putExtra(Intent.EXTRA_TEXT, txtMessage);
             sendIntent.setType("text/plain");
-            sendIntent.putExtra(Intent.EXTRA_TEXT,"");
-            sendIntent.putExtra("jid", txt +"@s.whatsapp.net");
             sendIntent.setPackage("com.whatsapp");
             startActivity(sendIntent);
-        }
-        catch(Exception e)
-        {
-            Toast.makeText(this,"Error/n"+ e.toString(),Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(MainActivity.this, "Whatsapp not instaled", Toast.LENGTH_SHORT).show();
         }
     }
+
+    void whatsappSend(String mob_number, String txtMessage) {
+        boolean installed = appInstalledOrNot("com.whatsapp");
+        if (installed) {
+            Intent intent = new Intent(Intent.ACTION_VIEW);
+            intent.setData(Uri.parse("http://api.whatsapp.com/send?phone="+ "+91"+ mob_number + "&text=" + txtMessage));
+            startActivity(intent);
+        } else {
+            Toast.makeText(MainActivity.this, "Whatsapp not instaled", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
 
 
     //Closes the App: Closing current activity
