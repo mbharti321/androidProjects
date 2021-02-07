1754b20c32cabafcc5f579f595f5b723cab0cc64
mbharti321 <mbharti321@gmail.com>
2021-02-07 13:57:56 +0530

    update

--- lab10ContextMenu/app/src/main/java/com/example/lab10contextmenu/MainActivity.java
+++ lab10ContextMenu/app/src/main/java/com/example/lab10contextmenu/MainActivity.java
@@ -3,12 +3,53 @@ package com.example.lab10contextmenu;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    ImageView m_imgJuice, m_imgDosa, m_imgPizza, m_imgPulav;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        m_imgJuice = findViewById(R.id.imgJuice);
+        m_imgDosa = findViewById(R.id.imgDosa);
+        m_imgPizza = findViewById(R.id.imgPizza);
+        m_imgPulav = findViewById(R.id.imgPulav);
+
+
+        m_imgJuice.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "juice", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        m_imgDosa.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Dosa", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+        m_imgPizza.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Pizza", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        m_imgPulav.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Pulav", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
+
+
 }
